2b5ab7e59ad98d64d6fbd002648542063d2f92af
perryaj <perry@perryjone.es>
2018-09-20 14:48:40 -0700

    wip, update versions and examples for 8

---
+++ buildAll.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mvn -f AbstractTagDriverExample/pom.xml clean install
+mvn -f ComponentExample/pom.xml clean install
+mvn -f expression-example/pom.xml clean install
+mvn -f gateway-network-example/pom.xml clean install
+mvn -f home-connect-example/pom.xml clean install
+mvn -f ModbusDriverExample/pom.xml clean install
+mvn -f NotificationExample/pom.xml clean install
+mvn -f report-component/pom.xml clean install
+mvn -f report-datasource-example/pom.xml clean install
+mvn -f scripting-rpc-example/pom.xml clean install
+mvn -f SimpleTagProviderExample/pom.xml clean install
+mvn -f WeatheModuleExample/pom.xml clean install
