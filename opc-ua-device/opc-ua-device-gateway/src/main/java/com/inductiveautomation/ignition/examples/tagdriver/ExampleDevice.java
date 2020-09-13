309fa0642b62f093c3adff050022e311d4dac2d7
Kevin Herron <kevinherron@gmail.com>
2020-09-13 05:10:52 -0700

    Update opc-ua-device example for 8.1 Device API changes (#43)
    Milo 0.5.0 removes everything marked deprecated from 0.1 - 0.4 and tweaks the class hierarchy of managed AddressSpace implementations such that AbstractLifecycle is no longer at the root of it.
    
    Device API gains ManagedDevice as the go-to "easy mode" device base class.

--- opc-ua-device/opc-ua-device-gateway/src/main/java/com/inductiveautomation/ignition/examples/tagdriver/ExampleDevice.java
+++ opc-ua-device/opc-ua-device-gateway/src/main/java/com/inductiveautomation/ignition/examples/tagdriver/ExampleDevice.java
@@ -1,56 +1,53 @@
 package com.inductiveautomation.ignition.examples.tagdriver;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 
 import com.inductiveautomation.ignition.examples.tagdriver.configuration.ExampleDeviceType;
 import com.inductiveautomation.ignition.examples.tagdriver.configuration.ValueSimulator;
 import com.inductiveautomation.ignition.examples.tagdriver.configuration.settings.ExampleDeviceSettings;
-import com.inductiveautomation.ignition.gateway.opcua.server.api.Device;
 import com.inductiveautomation.ignition.gateway.opcua.server.api.DeviceContext;
+import com.inductiveautomation.ignition.gateway.opcua.server.api.DeviceType;
+import com.inductiveautomation.ignition.gateway.opcua.server.api.ManagedDevice;
 import org.eclipse.milo.opcua.sdk.core.AccessLevel;
 import org.eclipse.milo.opcua.sdk.core.Reference;
 import org.eclipse.milo.opcua.sdk.server.api.DataItem;
-import org.eclipse.milo.opcua.sdk.server.api.ManagedAddressSpaceServices;
 import org.eclipse.milo.opcua.sdk.server.api.MonitoredItem;
-import org.eclipse.milo.opcua.sdk.server.api.nodes.VariableNode;
-import org.eclipse.milo.opcua.sdk.server.nodes.AttributeContext;
 import org.eclipse.milo.opcua.sdk.server.nodes.UaFolderNode;
 import org.eclipse.milo.opcua.sdk.server.nodes.UaVariableNode;
-import org.eclipse.milo.opcua.sdk.server.nodes.delegates.AttributeDelegate;
+import org.eclipse.milo.opcua.sdk.server.nodes.filters.AttributeFilters;
 import org.eclipse.milo.opcua.sdk.server.util.SubscriptionModel;
 import org.eclipse.milo.opcua.stack.core.BuiltinDataType;
 import org.eclipse.milo.opcua.stack.core.Identifiers;
 import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UByte;
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned;
 
-public class ExampleDevice extends ManagedAddressSpaceServices implements Device {
+public class ExampleDevice extends ManagedDevice {
 
-    private static final UByte READ_ONLY = Unsigned.ubyte(AccessLevel.getMask(AccessLevel.READ_ONLY));
-    private static final UByte READ_WRITE = Unsigned.ubyte(AccessLevel.getMask(AccessLevel.READ_WRITE));
-
+    private final ValueSimulator simulator = new ValueSimulator();
     private final SubscriptionModel subscriptionModel;
 
     private final DeviceContext deviceContext;
     private final ExampleDeviceSettings settings;
 
-    public ExampleDevice(DeviceContext deviceContext, ExampleDeviceSettings settings) {
-        super(deviceContext.getServer());
+    public ExampleDevice(
+        DeviceType deviceType,
+        DeviceContext deviceContext,
+        ExampleDeviceSettings settings
+    ) {
+
+        super(deviceType, deviceContext);
 
         this.deviceContext = deviceContext;
         this.settings = settings;
 
         subscriptionModel = new SubscriptionModel(deviceContext.getServer(), this);
-    }
 
-    @Nonnull
-    @Override
-    public String getName() {
-        return deviceContext.getName();
+        getLifecycleManager().addStartupTask(this::onStartup);
+        getLifecycleManager().addShutdownTask(this::onShutdown);
     }
 
     @Nonnull
@@ -59,15 +56,13 @@ public class ExampleDevice extends ManagedAddressSpaceServices implements Device
         return "Running";
     }
 
-    @Nonnull
-    @Override
-    public String getTypeId() {
-        return ExampleDeviceType.TYPE_ID;
-    }
+    private void onStartup() {
+        subscriptionModel.startup();
 
-    @Override
-    public void onStartup() {
-        super.onStartup();
+        // create a basic tag updater service, let the gateway run it separately from UA subscription management
+        deviceContext.getGatewayContext()
+            .getExecutionManager()
+            .registerAtFixedRate(ExampleDeviceType.TYPE_ID, deviceContext.getName(), simulator, 1, TimeUnit.SECONDS);
 
         // create a folder node for our configured device
         UaFolderNode rootNode = new UaFolderNode(
@@ -88,8 +83,8 @@ public class ExampleDevice extends ManagedAddressSpaceServices implements Device
             Reference.Direction.INVERSE
         ));
 
-        addSimpleFolder(rootNode, "static", settings.getTagCount(), READ_WRITE);
-        addSimpleFolder(rootNode, "readOnly", settings.getTagCount(), READ_ONLY);
+        addSimpleFolder(rootNode, "static", settings.getTagCount(), AccessLevel.READ_WRITE);
+        addSimpleFolder(rootNode, "readOnly", settings.getTagCount(), AccessLevel.READ_ONLY);
 
         addDynamicNodes(rootNode);
 
@@ -100,10 +95,7 @@ public class ExampleDevice extends ManagedAddressSpaceServices implements Device
         onDataItemsCreated(dataItems);
     }
 
-    @Override
-    public void onShutdown() {
-        super.onShutdown();
-
+    private void onShutdown() {
         subscriptionModel.shutdown();
 
         deviceContext.getGatewayContext()
@@ -124,12 +116,6 @@ public class ExampleDevice extends ManagedAddressSpaceServices implements Device
         // addOrganizes is just a helper method to an OPC UA "Organizes" references to a folder node
         rootNode.addOrganizes(folder);
 
-        // create a basic tag updater service, let the gateway run it separately from UA subscription management
-        ValueSimulator simulator = new ValueSimulator();
-        deviceContext.getGatewayContext()
-            .getExecutionManager()
-            .registerAtFixedRate(ExampleDeviceType.TYPE_ID, deviceContext.getName(), simulator, 1, TimeUnit.SECONDS);
-
         for (int i = 0; i < settings.getTagCount(); i++) {
             String formattedName = String.format("%s%d", name, i);
             UaVariableNode node = UaVariableNode.builder(getNodeContext())
@@ -138,27 +124,26 @@ public class ExampleDevice extends ManagedAddressSpaceServices implements Device
                 .setDisplayName(new LocalizedText(formattedName))
                 .setDataType(BuiltinDataType.UInt32.getNodeId())
                 .setTypeDefinition(Identifiers.BaseDataVariableType)
-                .setAccessLevel(READ_ONLY)
-                .setUserAccessLevel(READ_ONLY)
+                .setAccessLevel(AccessLevel.READ_ONLY)
+                .setUserAccessLevel(AccessLevel.READ_ONLY)
                 .build();
 
             // just tells our simulator to keep track of this node
             simulator.addTrackedValue(formattedName, i);
 
-            // an attribute delegate is used so that when this node is asked for its value, it will call out to the
+            // an AttributeFilter is used so that when this node is asked for its value, it will call out to the
             // simulator
-            node.setAttributeDelegate(new AttributeDelegate() {
-                @Override
-                public DataValue getValue(AttributeContext context, VariableNode node) {
-                    return simulator.getTrackedValue(formattedName);
-                }
-            });
+            node.getFilterChain().addLast(AttributeFilters.getValue(
+                getAttributeContext ->
+                    simulator.getTrackedValue(formattedName))
+            );
+
             getNodeManager().addNode(node);
             folder.addOrganizes(node);
         }
     }
 
-    private void addSimpleFolder(UaFolderNode rootNode, String name, int count, UByte accessLevel) {
+    private void addSimpleFolder(UaFolderNode rootNode, String name, int count, Set<AccessLevel> accessLevel) {
         UaFolderNode folder = new UaFolderNode(
             getNodeContext(),
             deviceContext.nodeId(name),
