34fdc06610c4b35e55490a2ef5db8cf5170f8ec2
Vi Quang <vquang@inductiveautomation.com>
2025-06-13 16:00:43 -0700

    Update installation best practice.

--- event-stream-handler/event-stream-handler-gateway/src/main/java/com/inductiveautomation/ignition/examples/eventstream/handler/gateway/ExampleHandlerGatewayHook.java
+++ event-stream-handler/event-stream-handler-gateway/src/main/java/com/inductiveautomation/ignition/examples/eventstream/handler/gateway/ExampleHandlerGatewayHook.java
@@ -1,17 +1,25 @@
 package com.inductiveautomation.ignition.examples.eventstream.handler.gateway;
 
 import com.inductiveautomation.ignition.common.licensing.LicenseState;
+import com.inductiveautomation.ignition.common.util.LoggerEx;
 import com.inductiveautomation.ignition.examples.eventstream.handler.ExampleHandlerModule;
 import com.inductiveautomation.ignition.gateway.model.AbstractGatewayModuleHook;
 import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 import com.inductiveautomation.ignition.gateway.model.ModuleState;
 
 public class ExampleHandlerGatewayHook extends AbstractGatewayModuleHook {
+    private final LoggerEx logger = LoggerEx.newBuilder().build(ExampleHandlerGatewayHook.class);
 
     @Override
     public void setup(GatewayContext context) {
         if (eventStreamLoaded(context)) {
             EventStreamInstaller.setup(context);
+        } else {
+            var warningMessage = "The Event Stream module is not installed or not active. "
+                    + "The ExampleHandler will not be available. "
+                    + "Please install or enable the Event Stream module to use this module.";
+            logger.warn(warningMessage);
+            throw new IllegalStateException(warningMessage);
         }
     }
 
