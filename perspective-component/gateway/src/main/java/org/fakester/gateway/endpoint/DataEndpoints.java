be0817f32637d959af25a391a839e4e8048def9b
Vi Quang <vquang@inductiveautomation.com>
2025-06-20 09:33:50 -0700

    Update project to 8.3 sdk

--- perspective-component/gateway/src/main/java/org/fakester/gateway/endpoint/DataEndpoints.java
+++ perspective-component/gateway/src/main/java/org/fakester/gateway/endpoint/DataEndpoints.java
@@ -3,7 +3,6 @@ package org.fakester.gateway.endpoint;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
-import javax.servlet.http.HttpServletResponse;
 
 import com.inductiveautomation.ignition.common.gson.JsonObject;
 import com.inductiveautomation.ignition.common.tags.model.TagProvider;
@@ -13,6 +12,7 @@ import com.inductiveautomation.ignition.gateway.dataroutes.RequestContext;
 import com.inductiveautomation.ignition.gateway.dataroutes.RouteGroup;
 import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 import com.inductiveautomation.ignition.gateway.tags.model.GatewayTagManager;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 /**
@@ -21,15 +21,15 @@ import com.inductiveautomation.ignition.gateway.tags.model.GatewayTagManager;
 public final class DataEndpoints {
     // how long to hold cached value before allowing new count calculation
     private static final long CACHE_DURATION_MS = Long.getLong("RadComponents.TagCounter.CacheDuration", 30000L);
-    private static AtomicReference<Long> tagCount = new AtomicReference<>(0L);
-    private static AtomicReference<Long> timestampOfLastCount = new AtomicReference<>(0L);
+    private static final AtomicReference<Long> tagCount = new AtomicReference<>(0L);
+    private static final AtomicReference<Long> timestampOfLastCount = new AtomicReference<>(0L);
 
     private DataEndpoints() {
         // private constructor
     }
 
     public static void mountRoutes(RouteGroup routes) {
-        // creates a new data route reachable at host:port/main/data/radcomppnents/component/tagcount
+        // creates a new data route reachable at host:port/main/data/radcomponents/component/tagcount
         routes.newRoute("/component/tagcount")
             .type(RouteGroup.TYPE_JSON)
             .handler(DataEndpoints::fetchTagCount)
@@ -37,14 +37,14 @@ public final class DataEndpoints {
     }
 
     /**
-     * Returns a simple json object in form of
+     * Returns a simple JSON object in form of
      * <pre>
      *     {
      *         "tagCount": &lt;number&gt;
      *     }
      * </pre>
      *
-     * Tag counts are cached and re-caclcated no more often than {@link DataEndpoints#CACHE_DURATION_MS}.
+     * <p>Tag counts are cached and re-calculated no more often than {@link DataEndpoints#CACHE_DURATION_MS}.</p>
      *
      * Note: an 'ideal' implementation of this sort would require some additional concurrency handling for optimal
      * performance and safety, but was skipped to keep example focused on big picture functionality.
--- perspective-component/settings.gradle.kts
+++ perspective-component/settings.gradle.kts
@@ -19,6 +19,9 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://nexus.inductiveautomation.com/repository/public/")
         }
+        maven {
+            url = uri("https://nexus.inductiveautomation.com/repository/inductiveautomation-beta/")
+        }
 
         // Declare the Node.js download repository.  We do this here so that we can continue to have repositoryMode set
         // to 'PREFER SETTINGS', as the node plugin will respect that and not set the node repo, meaning we can't
